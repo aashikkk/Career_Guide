689f14a5c829a50d6b174855c975ff9172cbcdae
aashikkk <sihaabama@gmail.com>
2024-05-17 23:10:09 +0530

    user and counsellor can view their appointments

--- client/src/components/TableViewableCounsellor.jsx
+++ client/src/components/TableViewableCounsellor.jsx
@@ -1,61 +1,60 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Table } from "flowbite-react";
+import axios from "../axios";
+import { AuthContext } from "../auth/AuthContext";
 
-function ViewableTable() {
+function ViewableTableCounsellor() {
+	const { user } = useContext(AuthContext);
+	const [appointments, setAppointments] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("/appointment/")
+			.then((response) => {
+				setAppointments(response.data);
+			})
+			.catch((error) => {
+				console.error("Error fetching appointments", error);
+			});
+	}, [user]);
+
 	return (
-		<div className="overflow-x-auto px-15 py-12 ">
-			<Table striped>
-				<Table.Head>
-					<Table.HeadCell>Product name</Table.HeadCell>
-					<Table.HeadCell>Color</Table.HeadCell>
-					<Table.HeadCell>Category</Table.HeadCell>
-					<Table.HeadCell>Price</Table.HeadCell>
-				</Table.Head>
-				<Table.Body className="divide-y">
-					<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-							{'Apple MacBook Pro 17"'}
-						</Table.Cell>
-						<Table.Cell>Sliver</Table.Cell>
-						<Table.Cell>Laptop</Table.Cell>
-						<Table.Cell>$2999</Table.Cell>
-					</Table.Row>
-					<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-							Microsoft Surface Pro
-						</Table.Cell>
-						<Table.Cell>White</Table.Cell>
-						<Table.Cell>Laptop PC</Table.Cell>
-						<Table.Cell>$1999</Table.Cell>
-					</Table.Row>
-					<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-							Magic Mouse 2
-						</Table.Cell>
-						<Table.Cell>Black</Table.Cell>
-						<Table.Cell>Accessories</Table.Cell>
-						<Table.Cell>$99</Table.Cell>
-					</Table.Row>
-					<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-							Google Pixel Phone
-						</Table.Cell>
-						<Table.Cell>Gray</Table.Cell>
-						<Table.Cell>Phone</Table.Cell>
-						<Table.Cell>$799</Table.Cell>
-					</Table.Row>
-					<Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-						<Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-							Apple Watch 5
-						</Table.Cell>
-						<Table.Cell>Red</Table.Cell>
-						<Table.Cell>Wearables</Table.Cell>
-						<Table.Cell>$999</Table.Cell>
-					</Table.Row>
-				</Table.Body>
-			</Table>
+		<div className="overflow-x-auto px-15 py-12 mx-auto w-11/12">
+			{appointments.length > 0 &&
+			appointments.some(
+				(appointment) => appointment.resourcePerson === user.name
+			) ? (
+				<Table striped>
+					<Table.Head>
+						{Object.keys(appointments[0]).map(
+							(key) =>
+								!["id", "resourcePerson"].includes(key) && (
+									<Table.HeadCell key={key}>{key}</Table.HeadCell>
+								)
+						)}
+					</Table.Head>
+					<Table.Body className="divide-y">
+						{appointments
+							.filter((appointment) => appointment.resourcePerson === user.name)
+							.map((appointment) => (
+								<Table.Row
+									key={appointment.id}
+									className="bg-white dark:border-gray-700 dark:bg-gray-800">
+									{Object.keys(appointment).map(
+										(key) =>
+											!["id", "resourcePerson"].includes(key) && (
+												<Table.Cell key={key}>{appointment[key]}</Table.Cell>
+											)
+									)}
+								</Table.Row>
+							))}
+					</Table.Body>
+				</Table>
+			) : (
+				<p>No appointment found for you.</p>
+			)}
 		</div>
 	);
 }
 
-export default ViewableTable;
+export default ViewableTableCounsellor;
